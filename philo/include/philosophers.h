f430a2a8687f86a55e8e2c470d8098ec502b2038
biraj-phuyal <birajphuyal11@gmail.com>
2025-11-05 19:45:08 +0000

    begining the project

---
+++ philo/include/philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 18:34:59 by biphuyal          #+#    #+#             */
+/*   Updated: 2025/11/05 18:35:14 by biphuyal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <sys/time.h>
+# include <stdbool.h>
+# include <pthread.h>
+
+#endif
