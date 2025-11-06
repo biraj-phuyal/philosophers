70a22dec830554e743da2774c5ae51c43e9fbe2c
biraj-phuyal <birajphuyal11@gmail.com>
2025-11-06 00:13:43 +0000

    Checks for arguments

--- philo/core/checks.c
+++ philo/core/checks.c
@@ -1,23 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.c                                     :+:      :+:    :+:   */
+/*   checks.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/05 17:56:46 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/11/06 00:13:12 by biphuyal         ###   ########.fr       */
+/*   Created: 2025/11/06 00:04:57 by biphuyal          #+#    #+#             */
+/*   Updated: 2025/11/06 00:12:54 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosophers.h"
 
-int main(int args, char **argv)
+bool	check_positiveness(char **argv)
 {
-	pthread_t *philosophers;
-	
-	if (!check_args(args))
-		return (1);
-	if (!check_positiveness(argv))
-		return (1);
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	while(argv[j])
+	{
+		while (argv[j][i])
+		{
+			if (argv[j][i] < 1)
+			{
+				write(1, "Arguments must be above 0\n", 24);
+				return (false);
+			}
+			i++;
+		}
+		j++;
+	}
+	return (true);
+}
+
+bool	check_args(int args)
+{
+	if (args != 6)
+	{
+		write(1, "Only 5 arguments are accepted\n", 30);
+			return (false);
+	}
+	return(true);
 }
--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 18:34:59 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/11/05 18:35:14 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/11/06 00:13:03 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,20 @@
 # include <sys/time.h>
 # include <stdbool.h>
 # include <pthread.h>
+
+typedef struct	s_philosophers
+{
+	void	*fork;
+	void	*eat;
+	void	*sleeps;
+	void	*thinks;
+	void	*time_to_eat;
+	void	*time_to_sleep;
+	void	*time_to_think;
+	pthread_t thread;
+}	t_philosophers;
+
+bool	check_args(int args);
+bool	check_positiveness(char **argv);
 
 #endif
