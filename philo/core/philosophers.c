7a6d7e718b42e95f7fd9bb3667ffc06c52e1dfc3
biraj-phuyal <birajphuyal11@gmail.com>
2026-01-14 20:22:25 +0000

    just learning

--- philo/core/philosophers.c
+++ philo/core/philosophers.c
@@ -6,11 +6,15 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 17:56:46 by biphuyal          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2025/11/11 18:45:28 by biphuyal         ###   ########.fr       */
+=======
+/*   Updated: 2026/01/14 20:21:02 by biphuyal         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/philosophers.h"
+#include <philosophers.h>
 
 void create_philisophers(t_philosophers *philosophers, char **argv)
 {
@@ -20,10 +24,20 @@ void create_philisophers(t_philosophers *philosophers, char **argv)
 
 int main(int args, char **argv)
 {
+<<<<<<< Updated upstream
 	pthread_t philosophers;
 	
 	if (!check_all_posibility(args, argv))
 		return (1);
 	create_philisophers(&philosophers, argv);
 	
+=======
+	static t_philo	philosophers;
+	
+	if (!check_all_posibility(args, argv))
+		return (1);
+	if (!start(args, argv, &philosophers));
+		return (1);
+
+>>>>>>> Stashed changes
 }
--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -6,7 +6,11 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 18:34:59 by biphuyal          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2025/11/11 17:15:40 by biphuyal         ###   ########.fr       */
+=======
+/*   Updated: 2026/01/14 20:21:25 by biphuyal         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +22,7 @@
 # include <stdbool.h>
 # include <pthread.h>
 
+<<<<<<< Updated upstream
 typedef long long t_ms;
 
 typedef struct	s_philosophers
@@ -31,7 +36,29 @@ typedef struct	s_philosophers
 	void		*time_to_think;
 	pthread_t	thread;
 }	t_philosophers;
+=======
+typedef struct	s_ms t_ms;
+
+typedef struct	s_data
+{
+	pthread_t		*fork;
+	pthread_t		*eat;
+	pthread_t		*sleep;
+	void			*time_to_eat;
+	void			*time_to_sleep;
+	void			*time_to_think;
+	int				must_eat;
+	
+}	t_data;
+
+typedef struct	s_philo
+{
+	t_data		*data;
+	pthread_t	thread;
+}	t_philo;
+>>>>>>> Stashed changes
 
 bool	check_all_posibility(int args, char **argv);
+int		start(int args, char **argv, t_philo **philo);
 
 #endif
