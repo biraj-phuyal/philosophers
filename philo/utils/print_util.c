b3be9af466a1e7d1305e40167124631bb4bcc087
biraj-phuyal <birajphuyal11@gmail.com>
2026-04-22 15:46:06 +0100

    Removing uneccessary funtion

--- philo/utils/print_util.c
+++ philo/utils/print_util.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 17:52:05 by biphuyal          #+#    #+#             */
-/*   Updated: 2026/04/17 17:52:05 by biphuyal         ###   ########.fr       */
+/*   Updated: 2026/04/21 17:27:39 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void	print_death(t_philo *philo)
 	if (!simulation_stopped(philo->data))
 	{
 		printf("%ld %d died\n", get_timestamp(philo), philo->id);
-		fflush(stdout);
 		set_simulation_stop(philo->data, true);
 	}
 	pthread_mutex_unlock(&philo->data->print_lock);
@@ -34,9 +33,6 @@ void	print_status(t_philo *philo, const char *status)
 {
 	pthread_mutex_lock(&philo->data->print_lock);
 	if (!simulation_stopped(philo->data))
-	{
 		printf("%ld %d %s\n", get_timestamp(philo), philo->id, status);
-		fflush(stdout);
-	}
 	pthread_mutex_unlock(&philo->data->print_lock);
 }
